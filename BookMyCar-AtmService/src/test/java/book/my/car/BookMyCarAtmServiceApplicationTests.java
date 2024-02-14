0cf3c302e0a3f32ce22833dcb15cdedb3a094378
krkhengte <152171772+krkhengte@users.noreply.github.com>
2024-02-14 10:38:46 +0530

    Update BookMyCarAtmServiceApplicationTests.java

--- BookMyCar-AtmService/src/test/java/book/my/car/BookMyCarAtmServiceApplicationTests.java
+++ BookMyCar-AtmService/src/test/java/book/my/car/BookMyCarAtmServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package book.my.car;
+// package book.my.car;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookMyCarAtmServiceApplicationTests {
+// @SpringBootTest
+// class BookMyCarAtmServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
