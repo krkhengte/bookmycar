57f4362c9f512236319cee58a47e85b11492c4e3
krkhengte <152171772+krkhengte@users.noreply.github.com>
2024-02-14 10:43:49 +0530

    Update BookMyCarDriverServiceApplicationTests.java

--- BookMyCar-DriverService/src/test/java/book/my/car/BookMyCarDriverServiceApplicationTests.java
+++ BookMyCar-DriverService/src/test/java/book/my/car/BookMyCarDriverServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package book.my.car;
+// package book.my.car;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookMyCarDriverServiceApplicationTests {
+// @SpringBootTest
+// class BookMyCarDriverServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
