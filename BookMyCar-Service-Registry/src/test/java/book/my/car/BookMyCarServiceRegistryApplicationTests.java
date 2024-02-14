b832f0aff4fa55677ac384f259b1af03d6b5df4c
krkhengte <152171772+krkhengte@users.noreply.github.com>
2024-02-14 10:47:01 +0530

    Update BookMyCarServiceRegistryApplicationTests.java

--- BookMyCar-Service-Registry/src/test/java/book/my/car/BookMyCarServiceRegistryApplicationTests.java
+++ BookMyCar-Service-Registry/src/test/java/book/my/car/BookMyCarServiceRegistryApplicationTests.java
@@ -1,13 +1,13 @@
-package book.my.car;
+// package book.my.car;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookMyCarServiceRegistryApplicationTests {
+// @SpringBootTest
+// class BookMyCarServiceRegistryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
